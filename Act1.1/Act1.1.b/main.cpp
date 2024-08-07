e3fe8045bbe7c899ce3a3c65e181cbfb3683898b
Aaron3312 <a01642529@tec.mx>
2024-08-07 15:14:54 -0600

    cambios menorescpp

---
+++ Act1.1/Act1.1.b/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Funcion para ordenar con el algorithm sort de la biblioteca estandar de C++ 
+
+struct student {
+
+  int age;
+
+ string name;
+
+};
+
+int main() {
+  // Crear un vector de estudiantes
+  vector<student> students = {
+    {20, "Juan"},
+    {21, "Pedro"},
+    {19, "Maria"},
+    {22, "Ana"},
+    {18, "Luis"}
+  };
+
+  // Ordenar el vector de estudiantes por edad en forma ascendente
+  sort(students.begin(), students.end(), [](student a, student b) {
+    return a.age < b.age;
+  });
+
+  // Imprimir el vector de estudiantes ordenado
+
+  for (student s : students) {
+    cout << s.age << " " << s.name << endl;
+  }
+  
+
+  return 0;
+}
