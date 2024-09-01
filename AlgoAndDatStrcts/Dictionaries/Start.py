f8d7cfec2122765c5fb038ccae661c6eece6ed04
Aaron Hernandez <aypierre223@gmail.com>
2024-09-01 01:56:47 -0600

    startign dead kiler

---
+++ AlgoAndDatStrcts/Dictionaries/Start.py
@@ -0,0 +1,30 @@
+myDict = {"miller": "killer the one who  works in the work"}
+
+
+my_dict = dict()
+
+print(my_dict)
+
+eng_sp = dict(one= "uno", two = "dos", three = "tres" )
+print(eng_sp)
+
+eng_sp2 = {"one":"uno", "two": "dos", "three": "tres"}
+print(eng_sp2)
+
+
+eng_sp3 = [("one", "uno"), ("two","dos"),("three","tres")]
+print(eng_sp3)
+
+engspLst = dict(eng_sp3)
+print(engspLst)
+
+
+eng_sp2["one"] = "onos"
+
+print(eng_sp2)
+
+
+def traverseDict(dict):
+    for key in dict:
+        print(key, dict[key])
+traverseDict(eng_sp2)
